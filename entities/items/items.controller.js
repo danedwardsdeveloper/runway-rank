0694505ad822e688500d6503af5734fbd6028d90
Dan Edwards <daniel.edwards96@yahoo.com>
2024-06-11 15:53:03 +0100

    Implement postRating route

--- entities/items/items.controller.js
+++ entities/items/items.controller.js
@@ -1,5 +1,7 @@
 const pool = require("../../pool.js");
 
+const { calculateWholePairs } = require("./utils.js");
+
 const getAllItems = async (req, res) => {
   try {
     const data = await pool.query("SELECT * FROM items");
@@ -10,13 +12,57 @@ const getAllItems = async (req, res) => {
   }
 };
 
-const getTotalItems = async (req, res) => {
+const getTotalPairs = async (req, res) => {
   try {
-    const data = await pool.query("SELECT COUNT(*) AS total FROM items");
-    res.json({ total: data.rows[0].total });
+    let data = await pool.query("SELECT COUNT(*) AS total FROM items");
+    let totalCount = data.rows[0].total;
+    let wholePairs = calculateWholePairs(totalCount);
+    res.json(wholePairs);
   } catch (error) {
     console.error("Error fetching items:", error);
-    return res.status(500).json({ message: "Error getting items!" });
+    return res.status(500).json({ message: "Error getting the total number of pairs" });
+  }
+};
+
+const getPairsRated = async (req, res) => {
+  try {
+    let data = await pool.query(`SELECT COUNT(*) AS total FROM items WHERE num_of_ratings > 0`);
+    let totalCount = data.rows[0].total;
+    let wholePairs = calculateWholePairs(totalCount);
+    res.json(wholePairs);
+  } catch (error) {
+    console.error("Error fetching items:", error);
+    return res.status(500).json({ message: "Error getting the number of rated pairs" });
+  }
+};
+
+const postRatings = async (req, res) => {
+  try {
+    const { winner } = req.body;
+    if (!winner) {
+      return res.status(400).json({ message: "Request body missing required field(s)" });
+    }
+
+    const updateWinner = await pool.query(
+      `UPDATE items
+      SET num_of_ratings = num_of_ratings + 1,
+          average_score = (average_score + 10)
+      WHERE id = $1`,
+      [winner]
+    );
+
+    res.json({ message: "Ratings updated successfully" });
+  } catch (error) {
+    console.error("Error updating ratings:", error);
+    return res.status(500).json({ message: "Error posting rating" });
+  }
+};
+
+const getNextPair = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error("Error fetching items:", error);
+    return res.status(500).json({ message: "Error getting next pair" });
   }
 };
 
@@ -26,12 +72,15 @@ const getTop10 = async (req, res) => {
     res.json(data);
   } catch (error) {
     console.error("Error fetching items:", error);
-    return res.status(500).json({ message: "Error getting items!" });
+    return res.status(500).json({ message: "Error getting items." });
   }
 };
 
 module.exports = {
   getAllItems,
-  getTotalItems,
+  getTotalPairs,
+  getPairsRated,
+  postRatings,
+  // getNextPair,
   getTop10,
 };
--- entities/items/items.router.js
+++ entities/items/items.router.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const itemsRouter = express.Router();
 
-const { getAllItems, getTotalItems, getTop10 } = require("./items.controller.js");
-const { calculateWholePairs } = require("./utils.js");
+const { getAllItems, getTotalPairs, getPairsRated, postRatings, getTop10 } = require("./items.controller.js");
 
 itemsRouter.get("/items", getAllItems);
-itemsRouter.get("/items/total", getTotalItems);
+itemsRouter.get("/items/whole-pairs", getTotalPairs);
+itemsRouter.get("/items/pairs-rated", getPairsRated);
+itemsRouter.post("/items/post-ratings", postRatings);
 itemsRouter.get("/items/top-10", getTop10);
 
 module.exports = itemsRouter;
