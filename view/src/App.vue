dd305cd6f12e04e842c4478199fcbf16a2cbc379
Dan Edwards <daniel.edwards96@yahoo.com>
2024-06-19 18:21:00 +0100

    Create Footer component

--- view/src/App.vue
+++ view/src/App.vue
@@ -1,16 +1,19 @@
 <template>
     <MenuBarComponent />
-    <main class="bg-slate-100 flex flex-col h-screen">
+    <main class="bg-slate-100 flex flex-col min-h-screen">
         <router-view />
     </main>
+    <FooterComponent />
 </template>
 
 <script>
 import MenuBarComponent from './components/MenuBarComponent';
+import FooterComponent from './components/FooterComponent.vue';
 
 export default {
     components: {
         MenuBarComponent,
+        FooterComponent
     },
 };
 </script>
---
+++ view/src/components/FooterComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer class="bg-gray-800 w-full">
+        <ul class="flex justify-center py-5">
+            <li
+                class="px-3 rounded-md py-2 text-sm font-medium space-x-4 text-gray-300 hover:bg-gray-700 hover:text-white ">
+                Link</li>
+            <li
+                class="px-3 rounded-md py-2 text-sm font-medium space-x-4 text-gray-300 hover:bg-gray-700 hover:text-white ">
+                Link</li>
+            <li
+                class="px-3 rounded-md py-2 text-sm font-medium space-x-4 text-gray-300 hover:bg-gray-700 hover:text-white ">
+                Link</li>
+        </ul>
+    </footer>
+</template>
