f4b500be1825ee8f422d6df5b59697653ad9c428
Dan Edwards <daniel.edwards96@yahoo.com>
2024-06-19 18:01:57 +0100

    Create logged-out Top Ten view

--- view/src/components/TopTenComponent.vue
+++ view/src/components/TopTenComponent.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="container mx-auto py-8">
         <h1 class="text-3xl font-bold mb-6 text-center">Top Ten Drag Race Lewks</h1>
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 m-4">
+        <p v-if="!accessTopLewks" class="text-red-500 text-center"><a href="">Cast more votes to view the top ten.</a>
+        </p>
+        <div v-if="accessTopLewks" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 m-4">
             <div v-for="(item, index) in items" :key="item.id"
                 class="bg-white p-4 rounded-lg shadow-lg flex flex-col justify-between">
+
                 <img :src="`${baseUrl}${item.image_path}`" :alt="item.name" class="w-full h-auto rounded-md">
 
                 <div class="flex justify-between">
@@ -21,6 +24,31 @@
 
             </div>
         </div>
+
+        <div v-else class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 m-4">
+            <div v-for="index in 10" :key="index"
+                class="bg-white p-4 rounded-lg shadow-lg flex flex-col justify-between">
+
+                <img src="../assets/mystery-drag-queen.webp" alt="Mystery look" class="w-full h-auto rounded-md">
+
+                <div class="flex justify-between">
+                    <div>
+                        <h2 class="text-xl font-semibold mt-2">Queen Name</h2>
+                        <h2 class="text-m gray-700 mb-2">Lewk description</h2>
+                        <p class="text-gray-600">Score: ?</p>
+                        <p class="text-gray-600">Ratings: ?</p>
+                    </div>
+
+                    <div class="flex flex-col justify-end">
+                        <h2 class="text-6xl bold">{{ index + 1 }}</h2>
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
+
     </div>
 </template>
 
@@ -30,6 +58,7 @@ export default {
         return {
             items: [],
             baseUrl: process.env.NODE_ENV === "development" ? "http://localhost:3000/images/" : "http://www.runwayrank.com/images/",
+            accessTopLewks: false,
         };
     },
     mounted() {
