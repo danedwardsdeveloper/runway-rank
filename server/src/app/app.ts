347101677ddfa554bea481fb0a7acff451a6566f
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-20 20:18:46 +0300

    Add protectedRouter, cookieParser, validateToken

--- server/src/app/app.ts
+++ server/src/app/app.ts
@@ -1,11 +1,18 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 import { connectToDatabase } from './database/database.js';
 import publicRouter from './routes/publicRouter.js';
+import protectedRouter from './routes/protectedRouter.js';
+import validateToken from './middleware/validateToken.js';
 
 const app = express();
 connectToDatabase();
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.use('/', publicRouter);
+app.use('/', protectedRouter, validateToken);
 
 export default app;
