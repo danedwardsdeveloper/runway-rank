0b6bb42b19de0f575bd73739afb80a0ae38c5899
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-21 01:32:50 +0300

    Add postRankings route

--- server/src/app/routes/protectedRouter.ts
+++ server/src/app/routes/protectedRouter.ts
@@ -1,9 +1,10 @@
 import express, { Router } from 'express';
 
 import protectedRoute from './protectedRoutes/protectedRoute.js';
+import postRankings from './protectedRoutes/postRankings.js';
 
 const protectedRouter: Router = express.Router();
 
-protectedRouter.use('/', protectedRoute);
+protectedRouter.use('/', protectedRoute, postRankings);
 
 export default protectedRouter;
