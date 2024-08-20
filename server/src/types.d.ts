0d1e0e7cb272b259df0007105454b0556a3999e4
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-21 01:32:03 +0300

    Add interfaces from other files

--- server/src/types.d.ts
+++ server/src/types.d.ts
@@ -1,11 +1,24 @@
-import { ObjectId } from 'mongoose';
+import mongoose, { ObjectId } from 'mongoose';
 import { Request } from 'express';
 
-export interface IUser {
+export interface RunwayItem {
+	name: string;
+	queen_id: ObjectId;
+	queen_name: string;
+	franchise?: string;
+	season?: number;
+	episode?: number;
+	episode_name?: string;
+	score: number;
+	ratings_count: number;
+	image_url: string;
+}
+
+export interface User {
 	email: string;
 	hashed_password: string;
 	name: string;
-	ranked_runway_ids: ObjectId[];
+	ranked_runway_ids: mongoose.Types.ObjectId[];
 }
 
 export interface JwtPayload {
@@ -17,5 +30,28 @@ export interface CustomRequest extends Request {
 	user?: {
 		id: string;
 		email: string;
+		name: string;
+		accessTopRunways: boolean;
+	};
+	body: {
+		authenticated: boolean;
+		[key: string]: any;
 	};
 }
+
+export interface Ranking {
+	user: mongoose.Types.ObjectId;
+	type: 'score' | 'ratings';
+	runways: ObjectId[];
+}
+
+export interface Queen {
+	name: string;
+	former_name?: string;
+	runways: ObjectId[];
+}
+
+type ValidateTokenResponse = {
+	nextPair: [RunwayItem, RunwayItem];
+	authenticated: boolean;
+};
