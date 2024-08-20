ac6b935251ff9b383e2cdbbc080b9c58e4a911c2
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-20 17:59:47 +0300

    Create database.ts

---
+++ server/src/app/database/database.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+import { environment } from '../../environment.js';
+import { createIndexes } from './createIndexes.js';
+
+export async function connectToDatabase(): Promise<void> {
+	try {
+		await mongoose.connect(environment.MONGO_STRING);
+		environment.isDevelopment &&
+			console.log('MongoDB connected successfully');
+	} catch (error) {
+		console.error('MongoDB connection error:', error);
+		process.exit(1);
+	}
+
+	await createIndexes();
+}
