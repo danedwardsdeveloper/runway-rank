225ebaa47f8c90944bf625a9384534e0b2ad7aea
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-20 20:18:59 +0300

    Move interfaces to global file

--- server/src/app/database/models/User.ts
+++ server/src/app/database/models/User.ts
@@ -1,18 +1,12 @@
 import mongoose from 'mongoose';
-import { ObjectId } from 'mongoose';
 
-export interface IUser {
-	email: string;
-	hashed_password: string;
-	name: string;
-	runways_ranked: ObjectId[];
-}
+import { IUser } from 'src/types.js';
 
 const UserSchema = new mongoose.Schema<IUser>({
 	email: { type: String, unique: true, required: true },
 	hashed_password: { type: String, required: true },
 	name: { type: String, required: true },
-	runways_ranked: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Runway' }],
+	ranked_runway_ids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Runway' }],
 });
 
 export const User = mongoose.model<IUser>('User', UserSchema);
