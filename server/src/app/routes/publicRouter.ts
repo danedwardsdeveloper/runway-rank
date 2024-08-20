2fa3a83b6550f3cf719a02db0f7b96589b303569
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-21 01:28:07 +0300

    Add more routes

--- server/src/app/routes/publicRouter.ts
+++ server/src/app/routes/publicRouter.ts
@@ -3,9 +3,20 @@ import express, { Router } from 'express';
 import welcome from './publicRoutes/welcome.js';
 import topRunways from './publicRoutes/topRunways.js';
 import createAccount from './publicRoutes/createAccount.js';
+import initialData from './publicRoutes/initialData.js';
+import signIn from './publicRoutes/signIn.js';
+import getNextPair from './publicRoutes/getNextPair.js';
 
 const publicRouter: Router = express.Router();
 
-publicRouter.use('/', welcome, topRunways, createAccount);
+publicRouter.use(
+	'/',
+	welcome,
+	topRunways,
+	createAccount,
+	signIn,
+	getNextPair,
+	initialData
+);
 
 export default publicRouter;
