e965953f3e3f0ec761e47a54a9e4ed2ecd3d7fd5
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-20 20:19:11 +0300

    Create validateToken.ts

---
+++ server/src/app/middleware/validateToken.ts
@@ -0,0 +1,33 @@
+import { Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { CustomRequest } from 'src/types.js';
+
+const validateToken = async (
+	req: CustomRequest,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		const token = req.cookies.jwt;
+
+		if (!token) {
+			return next();
+		}
+
+		const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
+			id: string;
+			email: string;
+		};
+
+		req.user = {
+			id: decoded.id,
+			email: decoded.email,
+		};
+
+		next();
+	} catch (error) {
+		next();
+	}
+};
+
+export default validateToken;
