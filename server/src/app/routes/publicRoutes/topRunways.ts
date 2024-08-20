9ede9b06767308742afaa6c28dcda11677b6a3a3
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-20 18:00:16 +0300

    Create topRunways.ts

---
+++ server/src/app/routes/publicRoutes/topRunways.ts
@@ -0,0 +1,22 @@
+import express, { Router, Request, Response } from 'express';
+import { Runway } from '../../database/models/Runway.js';
+
+const topRunways: Router = express.Router();
+
+topRunways.get('/top-runways', async (req: Request, res: Response) => {
+	try {
+		const topRunways = await Runway.find()
+			.sort({ score: -1 })
+			.limit(10)
+			.select('name queen_name franchise season episode score image_url');
+
+		res.json(topRunways);
+	} catch (error) {
+		console.error('Error fetching top runways:', error);
+		res.status(500).json({
+			message: 'An error occurred while fetching top runways',
+		});
+	}
+});
+
+export default topRunways;
