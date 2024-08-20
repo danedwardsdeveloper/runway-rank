8e529e151a42f35a582e286dbf837cf84b436871
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-20 20:19:05 +0300

    Create generateToken.ts

---
+++ server/src/app/middleware/generateToken.ts
@@ -0,0 +1,28 @@
+import { Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { Types } from 'mongoose';
+
+import { environment } from 'src/environment.js';
+
+interface UserData {
+	_id: Types.ObjectId;
+	email: string;
+}
+
+export function generateToken(res: Response, userData: UserData): void {
+	const token = jwt.sign(
+		{
+			id: userData._id.toString(),
+			email: userData.email,
+		},
+		environment.JWT_SECRET,
+		{ expiresIn: '1h' }
+	);
+
+	res.cookie('jwt', token, {
+		httpOnly: true,
+		secure: environment.isProduction,
+		sameSite: 'none',
+		maxAge: 60 * 60 * 1000,
+	});
+}
