a5ca9c5b2d73c878b977035441e22a6498bcd41f
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-21 14:30:38 +0300

    Organise routes & apply middleware in correct order

--- server/src/app/routes/publicRouter.ts
+++ server/src/app/routes/publicRouter.ts
@@ -1,22 +1,18 @@
 import express, { Router } from 'express';
 
+import { validateToken } from '../middleware/jwtToken.js';
 import welcome from './publicRoutes/welcome.js';
-import topRunways from './publicRoutes/topRunways.js';
 import createAccount from './publicRoutes/createAccount.js';
-import initialData from './publicRoutes/initialData.js';
 import signIn from './publicRoutes/signIn.js';
+import topRunways from './publicRoutes/topRunways.js';
 import getNextPair from './publicRoutes/getNextPair.js';
+import signOut from './publicRoutes/signOut.js';
+import { checkTopRunwaysAccess } from '@/app/middleware/checkTopRunwaysAccess.js';
 
 const publicRouter: Router = express.Router();
 
-publicRouter.use(
-	'/',
-	welcome,
-	topRunways,
-	createAccount,
-	signIn,
-	getNextPair,
-	initialData
-);
+publicRouter.use('/', welcome, createAccount, signIn, signOut);
+publicRouter.use('/', validateToken, getNextPair);
+publicRouter.use('/', validateToken, checkTopRunwaysAccess, topRunways);
 
 export default publicRouter;
