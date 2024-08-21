a299c9c6de214190c9d381c321f2631e4f5cedf6
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-21 14:28:08 +0300

    Combine app.use statements

--- server/src/app/app.ts
+++ server/src/app/app.ts
@@ -4,15 +4,11 @@ import cookieParser from 'cookie-parser';
 import { connectToDatabase } from './database/database.js';
 import publicRouter from './routes/publicRouter.js';
 import protectedRouter from './routes/protectedRouter.js';
-import validateToken from './middleware/validateToken.js';
 
 const app = express();
 connectToDatabase();
 
-app.use(express.json());
-app.use(cookieParser());
-
-app.use('/', publicRouter);
-app.use('/', protectedRouter, validateToken);
+app.use(express.json(), cookieParser());
+app.use('/', publicRouter, protectedRouter);
 
 export default app;
