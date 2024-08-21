6a6fecaa51f965705a499ec72969c707e4181de6
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-21 11:56:43 +0300

    Move validateToken to protectedRouter

--- server/src/app/routes/protectedRoutes/protectedRoute.ts
+++ server/src/app/routes/protectedRoutes/protectedRoute.ts
@@ -1,20 +1,15 @@
 import express, { Router, Response } from 'express';
 
 import { CustomRequest } from '@/types.js';
-import validateToken from '@/app/middleware/validateToken.js';
 
 const protectedRoute: Router = express.Router();
 
-protectedRoute.get(
-	'/protected-route',
-	validateToken,
-	(req: CustomRequest, res: Response) => {
-		if (req.user) {
-			res.json({ message: `Welcome, ${req.user.email}!` });
-		} else {
-			res.status(401).json({ message: 'Not authenticated' });
-		}
+protectedRoute.get('/protected-route', (req: CustomRequest, res: Response) => {
+	if (req.user) {
+		res.json({ message: `Welcome, ${req.user.email}!` });
+	} else {
+		res.status(401).json({ message: 'Not authenticated' });
 	}
-);
+});
 
 export default protectedRoute;
