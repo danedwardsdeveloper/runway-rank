a0d34fe41128a3503b8fcf618cb25d025e9b0ab3
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-21 11:54:13 +0300

    Add index for ranked_runway_ids

--- server/src/app/database/createIndexes.ts
+++ server/src/app/database/createIndexes.ts
@@ -12,10 +12,13 @@ export async function createIndexes() {
 
 	try {
 		await db.collection('users').createIndex({ email: 1 }, { unique: true });
+		await db.collection('users').createIndex({ ranked_runway_ids: 1 });
+
 		await db.collection('runways').createIndex({ name: 1 });
 		await db.collection('runways').createIndex({ queen_name: 1 });
 		await db.collection('runways').createIndex({ score: -1 });
 		await db.collection('runways').createIndex({ ratings_count: 1 });
+
 		await db.collection('queens').createIndex({ name: 1 });
 
 		environment.isDevelopment && console.log('Indexes created successfully');
