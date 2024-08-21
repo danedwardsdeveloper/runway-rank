ed3fc63df299093e5bfb45e0600089959fc35c49
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-21 20:20:06 +0300

    Delete middleware that involved database interaction

--- server/src/app/middleware/checkTopRunwaysAccess.ts
+++ server/src/app/middleware/checkTopRunwaysAccess.ts
@@ -1,51 +1,51 @@
-import { Response, NextFunction } from 'express';
+// import { Response, NextFunction } from 'express';
 
-import { CustomRequest, TopRunwayAccessResponse } from '@/types.js';
-import { UserModel } from '@/app/database/models/User.js';
-import { RunwayModel } from '@/app/database/models/Runway.js';
+// import { CustomRequest, TopRunwayAccessResponse } from '@/types.js';
+// import { UserModel } from '@/app/database/models/User.js';
+// import { RunwayModel } from '@/app/database/models/Runway.js';
 
-export const checkTopRunwaysAccess = async (
-	req: CustomRequest,
-	res: Response,
-	next: NextFunction
-): Promise<void> => {
-	try {
-		const totalRunways = await RunwayModel.countDocuments();
+// export const checkTopRunwaysAccess = async (
+// 	req: CustomRequest,
+// 	res: Response,
+// 	next: NextFunction
+// ): Promise<void> => {
+// 	try {
+// 		const totalRunways = await RunwayModel.countDocuments();
 
-		const response: TopRunwayAccessResponse = {
-			accessTopRunways: false,
-			numRunwaysUntilAccess: totalRunways,
-		};
+// 		const response: TopRunwayAccessResponse = {
+// 			accessTopRunways: false,
+// 			numRunwaysUntilAccess: totalRunways,
+// 		};
 
-		if (!req.user) {
-			next();
-			return;
-		}
+// 		if (!req.user) {
+// 			next();
+// 			return;
+// 		}
 
-		const user = await UserModel.findById(req.user.id);
-		if (!user) {
-			next();
-			return;
-		}
+// 		const user = await UserModel.findById(req.user.id);
+// 		if (!user) {
+// 			next();
+// 			return;
+// 		}
 
-		const userRankedRunways = user.ranked_runway_ids.length;
-		response.accessTopRunways = userRankedRunways >= totalRunways;
-		response.numRunwaysUntilAccess = Math.max(
-			0,
-			totalRunways - userRankedRunways
-		);
+// 		const userRankedRunways = user.ranked_runway_ids.length;
+// 		response.accessTopRunways = userRankedRunways >= totalRunways;
+// 		response.numRunwaysUntilAccess = Math.max(
+// 			0,
+// 			totalRunways - userRankedRunways
+// 		);
 
-		await UserModel.findByIdAndUpdate(user._id, {
-			accessTopRunways: response.accessTopRunways,
-			numRunwaysUntilAccess: response.numRunwaysUntilAccess,
-		});
+// 		await UserModel.findByIdAndUpdate(user._id, {
+// 			accessTopRunways: response.accessTopRunways,
+// 			numRunwaysUntilAccess: response.numRunwaysUntilAccess,
+// 		});
 
-		req.user.accessTopRunways = response.accessTopRunways;
-		req.user.numRunwaysUntilAccess = response.numRunwaysUntilAccess;
+// 		req.user.accessTopRunways = response.accessTopRunways;
+// 		req.user.numRunwaysUntilAccess = response.numRunwaysUntilAccess;
 
-		next();
-	} catch (error) {
-		console.error('Error checking top runways access:', error);
-		next(error);
-	}
-};
+// 		next();
+// 	} catch (error) {
+// 		console.error('Error checking top runways access:', error);
+// 		next(error);
+// 	}
+// };
