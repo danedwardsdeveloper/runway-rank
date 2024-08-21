105d135c00bc454c70273cb9b7236fe40976445f
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-21 17:45:58 +0300

    Apply checkTopRunwayAccess to all routes

--- server/src/app/routes/publicRouter.ts
+++ server/src/app/routes/publicRouter.ts
@@ -1,18 +1,26 @@
 import express, { Router } from 'express';
 
 import { validateToken } from '../middleware/jwtToken.js';
+import { checkTopRunwaysAccess } from '@/app/middleware/checkTopRunwaysAccess.js';
 import welcome from './publicRoutes/welcome.js';
 import createAccount from './publicRoutes/createAccount.js';
 import signIn from './publicRoutes/signIn.js';
 import topRunways from './publicRoutes/topRunways.js';
 import getNextPair from './publicRoutes/getNextPair.js';
 import signOut from './publicRoutes/signOut.js';
-import { checkTopRunwaysAccess } from '@/app/middleware/checkTopRunwaysAccess.js';
 
 const publicRouter: Router = express.Router();
 
-publicRouter.use('/', welcome, createAccount, signIn, signOut);
-publicRouter.use('/', validateToken, getNextPair);
-publicRouter.use('/', validateToken, checkTopRunwaysAccess, topRunways);
+publicRouter.use(validateToken, checkTopRunwaysAccess);
 
-export default publicRouter;
+publicRouter.use(
+	'/',
+	welcome,
+	createAccount,
+	signIn,
+	signOut,
+	getNextPair,
+	topRunways
+);
+
+export default publicRouter as Router;
