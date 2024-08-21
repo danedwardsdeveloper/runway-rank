c2f324fddd4d5e76c1a2bfc25544832027b5344a
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-21 11:39:11 +0300

    Test for nextPair array

--- server/__tests__/app.test.ts
+++ server/__tests__/app.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect, beforeAll, afterAll } from 'vitest';
 import request from 'supertest';
 import app from '../src/app/app';
 
-describe('Express Server Tests', () => {
+describe('MongoDB Express Tests', () => {
 	let server;
 
 	beforeAll(() => {
@@ -13,9 +13,40 @@ describe('Express Server Tests', () => {
 		server.close(done);
 	});
 
-	it('GET / should return Welcome message', async () => {
+	it('should return Welcome message', async () => {
 		const response = await request(server).get('/');
 		expect(response.status).toBe(200);
 		expect(response.text).toBe('Welcome to the Runway Rank API');
+	});
+
+	it('should return nextPair without user object', async () => {
+		const response = await request(server).get('/get-next-pair');
+
+		expect(response.status).toBe(200);
+		expect(response.body).not.toHaveProperty('user');
+		expect(response.body).toHaveProperty('nextPair');
+		expect(Array.isArray(response.body.nextPair)).toBe(true);
+		expect(response.body.nextPair).toHaveLength(2);
+
+		const expectedProperties = [
+			'_id',
+			'name',
+			'queen_id',
+			'queen_name',
+			'franchise',
+			'season',
+			'episode',
+			'episode_name',
+			'score',
+			'ratings_count',
+			'image_url',
+		];
+
+		response.body.nextPair.forEach((item) => {
+			expect(typeof item).toBe('object');
+			expectedProperties.forEach((prop) => {
+				expect(item).toHaveProperty(prop);
+			});
+		});
 	});
 });
