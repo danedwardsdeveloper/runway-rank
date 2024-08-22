d584d1c5f5dbf2bc7d3dc8f043ac43badd44a918
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-22 19:16:19 +0300

    Style ImageContainer

--- client/src/components/RankRunways/components/ImageContainer.tsx
+++ client/src/components/RankRunways/components/ImageContainer.tsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
 
-import { imageInfo, ImageInfo } from '../_tempData';
+import { RunwayPhoto, runwayPhotos } from '../_tempData';
 
 export default function ImageContainer() {
-	const [selectedImages, setSelectedImages] = useState<ImageInfo[]>([]);
+	const [selectedImages, setSelectedImages] = useState<RunwayPhoto[]>([]);
 
 	const shuffleImages = () => {
-		const shuffled = [...imageInfo].sort(() => 0.5 - Math.random());
+		const shuffled = [...runwayPhotos].sort(() => 0.5 - Math.random());
 		setSelectedImages(shuffled.slice(0, 2));
 	};
 
@@ -16,32 +16,37 @@ export default function ImageContainer() {
 
 	return (
 		<div className="flex justify-center items-start p-4 w-full max-w-6xl mx-auto">
-			<div className="flex flex-col sm:flex-row justify-center items-start gap-8 w-full">
+			<div className="flex flex-col sm:flex-row justify-center items-stretch gap-20 sm:gap-8 w-full">
 				{selectedImages.map((img, index) => (
 					<div
 						key={index}
-						onClick={shuffleImages}
-						className={`w-full md:w-1/2 flex flex-col items-center md:items-start cursor-pointer transition-transform duration-300 ${
+						className={`w-full md:w-1/2 flex flex-col items-center md:items-start ${
 							index === 0 ? 'md:items-end' : 'md:items-start'
 						}`}
 					>
-						<div className="relative inline-block">
+						<div className="flex-grow flex flex-col justify-end mb-4">
 							<img
-								src={img.url}
-								alt={img.title}
-								className="w-full h-auto max-w-[400px] max-h-[400px] object-contain rounded-lg"
+								src={`/runways/${img.slug}.webp`}
+								alt={img.description}
+								onClick={shuffleImages}
+								className="w-full h-auto md:max-w-[400px] md:max-h-[400px] object-contain cursor-pointer rounded-lg border-4 border-transparent hover:border-pink-500 transition-all duration-200"
 							/>
-							<div className="absolute inset-0 transition-all duration-200 border-4 border-transparent hover:border-pink-500 rounded-lg pointer-events-none" />
-
-							<div
-								className={`mt-4 ${
-									index === 0 ? 'md:text-right' : 'md:text-left'
-								}`}
-							></div>
-							<h2 className="text-xl font-bold">{img.title}</h2>
-							<h3 className="text-lg text-gray-600">{img.subtitle}</h3>
-							<p className="text-sm text-gray-500 mt-1">{img.model}</p>
-							<p className="text-sm mt-2">{img.description}</p>
+						</div>
+						<div
+							className={`w-full max-w-[400px] md:min-h-32 ${
+								index === 0 ? 'md:text-right' : 'md:text-left'
+							}`}
+						>
+							<h2 className="text-xl font-bold">{img.queenName}</h2>
+							<h3 className="text-lg text-gray-600">
+								{img.description}
+							</h3>
+							<p className="text-sm mt-2">
+								{img.franchise}
+								{img.season && ` S${img.season}`}
+								{img.episode && ` E${img.episode}`}
+							</p>
+							<p className="text-sm mt-2">{img.episodeName}</p>
 						</div>
 					</div>
 				))}
