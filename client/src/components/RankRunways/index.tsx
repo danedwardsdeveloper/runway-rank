73089e89828b5ca4b75509f9a23b524da1f499bd
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-22 16:53:36 +0300

    Remove unnecessary wrapper

--- client/src/components/RankRunways/index.tsx
+++ client/src/components/RankRunways/index.tsx
@@ -1,10 +1,5 @@
 import ImageContainer from './components/ImageContainer';
 
 export default function Home() {
-	return (
-		<div>
-			<h1 className="text-3xl text-center m-4">Rank Runways</h1>
-			<ImageContainer />
-		</div>
-	);
+	return <ImageContainer />;
 }
