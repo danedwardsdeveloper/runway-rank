c346505e252a072e0c4e986eb80e4a3db6d2efbd
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-23 10:57:11 +0300

    Serve public directory, add cors middleware

--- server/src/app/app.ts
+++ server/src/app/app.ts
@@ -1,14 +1,23 @@
 import express from 'express';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import { connectToDatabase } from './database/database.js';
+import { corsOptions } from './middleware/cors.js';
 import publicRouter from './routes/publicRouter.js';
 import protectedRouter from './routes/protectedRouter.js';
 
 const app = express();
 connectToDatabase();
 
-app.use(express.json(), cookieParser());
+app.use(express.static(path.join(__dirname, '../../public')));
+
+app.use(express.json(), cookieParser(), cors(corsOptions));
 app.use('/', publicRouter, protectedRouter);
 
 export default app;
