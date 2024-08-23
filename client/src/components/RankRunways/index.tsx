9aba2003f2ef3e61b76d765fcee59f33439bdedc
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-23 10:55:26 +0300

    Add http request to get data

--- client/src/components/RankRunways/index.tsx
+++ client/src/components/RankRunways/index.tsx
@@ -1,5 +1,38 @@
+import { useState, useEffect } from 'react';
+
 import ImageContainer from './components/ImageContainer';
+import { NextPairResponse } from '../../../../types';
+import { initialPair } from './_tempData';
 
 export default function Home() {
-	return <ImageContainer />;
+	const [nextPair, setNextPair] = useState<NextPairResponse>(initialPair);
+
+	const fetchNextPair = async () => {
+		try {
+			const response = await fetch('http://localhost:3000/get-next-pair', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+			if (!response.ok) {
+				throw new Error('Failed to fetch next pair');
+			}
+			const data: NextPairResponse = await response.json();
+			setNextPair(data);
+			console.log(data);
+		} catch (error) {
+			console.error('Error fetching next pair:', error);
+		}
+	};
+
+	useEffect(() => {
+		fetchNextPair();
+	}, []);
+
+	return (
+		<div>
+			<ImageContainer nextPair={nextPair} />
+		</div>
+	);
 }
