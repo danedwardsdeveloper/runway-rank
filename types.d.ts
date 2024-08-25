1170bcfc50cf142022b53e3413f624383f51ea69
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-25 09:07:55 +0100

    Add client types

--- types.d.ts
+++ types.d.ts
@@ -32,3 +32,33 @@ export interface UserBase {
 	accessTopRunways: boolean;
 	numRunwaysUntilAccess: number;
 }
+
+export interface UpdateUserOptions {
+	userId: string;
+	newRunwayIds?: string[];
+}
+
+export interface MetadataContent {
+	siteName: string;
+	defaultTitle: string;
+	defaultDescription: string;
+	author: string;
+	siteUrl: string;
+	defaultImage: string;
+}
+
+export interface MetadataProps {
+	title?: string;
+	description?: string;
+	pageName?: string;
+	slug?: string;
+}
+
+interface MenuItem {
+	name: string;
+	to: string;
+}
+
+export interface MenuItemsArray {
+	menuItems: MenuItem[];
+}
