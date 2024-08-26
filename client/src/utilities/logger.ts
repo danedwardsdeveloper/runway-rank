db6c110a67967675f81b6cfb580a2712de4db8af
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-26 08:35:47 +0100

    Create logger.ts

---
+++ client/src/utilities/logger.ts
@@ -0,0 +1,46 @@
+enum LogLevel {
+	ERROR = 0,
+	WARN = 1,
+	INFO = 2,
+	DEBUG = 3,
+}
+
+class Logger {
+	private level: LogLevel;
+
+	constructor(level: LogLevel = LogLevel.INFO) {
+		this.level = level;
+	}
+
+	error(message: string, ...args: any[]) {
+		if (this.level >= LogLevel.ERROR) {
+			console.error(message, ...args);
+		}
+	}
+
+	warn(message: string, ...args: any[]) {
+		if (this.level >= LogLevel.WARN) {
+			console.warn(message, ...args);
+		}
+	}
+
+	info(message: string, ...args: any[]) {
+		if (this.level >= LogLevel.INFO) {
+			console.info(message, ...args);
+		}
+	}
+
+	debug(message: string, ...args: any[]) {
+		if (this.level >= LogLevel.DEBUG) {
+			console.debug(message, ...args);
+		}
+	}
+
+	setLevel(level: LogLevel) {
+		this.level = level;
+	}
+}
+
+export const logger = new Logger(
+	process.env.NODE_ENV === 'production' ? LogLevel.ERROR : LogLevel.DEBUG
+);
