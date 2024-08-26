4523b401ef070b493416c5aac9652cfaa04a8d16
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-26 08:37:10 +0100

    Use logger, limiter & helmet

--- server/src/app/app.ts
+++ server/src/app/app.ts
@@ -1,4 +1,10 @@
-import express from 'express';
+import express, {
+	Request,
+	Response,
+	NextFunction,
+	ErrorRequestHandler,
+} from 'express';
+import helmet from 'helmet';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import path from 'path';
@@ -11,13 +17,34 @@ import { connectToDatabase } from './database/database.js';
 import { corsOptions } from './middleware/cors.js';
 import publicRouter from './routes/publicRouter.js';
 import protectedRouter from './routes/protectedRouter.js';
+import expressWinston from 'express-winston';
+import { logger, expressLogger } from './middleware/logger.js';
+import { limiter } from './middleware/limiter.js';
 
 const app = express();
 connectToDatabase();
-
+app.use(expressLogger, helmet()), limiter;
 app.use(express.static(path.join(__dirname, '../../public')));
 
 app.use(express.json(), cookieParser(), cors(corsOptions));
 app.use('/', publicRouter, protectedRouter);
+
+app.use(
+	expressWinston.errorLogger({
+		winstonInstance: logger,
+	})
+);
+
+const errorHandler: ErrorRequestHandler = (
+	err: Error,
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	logger.error(err.stack);
+	res.status(500).send('Unknown error!');
+};
+
+app.use(errorHandler);
 
 export default app;
