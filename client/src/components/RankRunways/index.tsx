58af60e0317c9c5eeb3952d3746ca03aed5a6fbb
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-26 08:35:05 +0100

    Add onClick function

--- client/src/components/RankRunways/index.tsx
+++ client/src/components/RankRunways/index.tsx
@@ -1,19 +1,24 @@
 import { useState, useEffect } from 'react';
 
 import ImageContainer from './components/ImageContainer';
-import { NextPairResponse } from '../../../../types';
+import { NextPairResponse, RunwayItem } from '../../../../types';
 import { initialPair } from './_tempData';
 
 export default function Home() {
 	const [nextPair, setNextPair] = useState<NextPairResponse>(initialPair);
 
-	const fetchNextPair = async () => {
+	const fetchNextPair = async (winner: string, loser: string) => {
 		try {
+			const body = JSON.stringify({ winner, loser });
+			console.log(body);
+
 			const response = await fetch('http://localhost:3000/get-next-pair', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
 				},
+				body: body,
+				credentials: 'include',
 			});
 			if (!response.ok) {
 				throw new Error('Failed to fetch next pair');
@@ -26,13 +31,28 @@ export default function Home() {
 		}
 	};
 
+	const handleImageClick = (clickedItem: RunwayItem) => {
+		if (nextPair.nextPair && nextPair.nextPair.length === 2) {
+			const winner = clickedItem._id;
+			const loser = nextPair.nextPair.find(
+				(item) => item._id !== winner
+			)?._id;
+			if (loser) {
+				fetchNextPair(winner, loser);
+			}
+		}
+	};
+
 	useEffect(() => {
-		fetchNextPair();
+		fetchNextPair('', '');
 	}, []);
 
 	return (
 		<div>
-			<ImageContainer nextPair={nextPair} />
+			<ImageContainer
+				nextPairResponse={nextPair}
+				onImageClick={handleImageClick}
+			/>
 		</div>
 	);
 }
