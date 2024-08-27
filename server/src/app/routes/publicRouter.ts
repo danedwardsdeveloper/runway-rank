a53219a2ecb5fed1b5b1a1bdafdba7482fbac90e
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-27 15:18:09 +0100

    Add 'profile' route

--- server/src/app/routes/publicRouter.ts
+++ server/src/app/routes/publicRouter.ts
@@ -4,10 +4,10 @@ import { validateToken } from '../middleware/jwtToken.js';
 import welcome from './publicRoutes/welcome.js';
 import createAccount from './publicRoutes/createAccount.js';
 import signIn from './publicRoutes/signIn.js';
-import topRunways from './publicRoutes/topRunways.js';
 import getNextPair from './publicRoutes/getNextPair.js';
 import signOut from './publicRoutes/signOut.js';
 import images from './publicRoutes/images.js';
+import profile from './publicRoutes/profile.js';
 
 const publicRouter: Router = express.Router();
 
@@ -20,7 +20,7 @@ publicRouter.use(
 	signIn,
 	signOut,
 	getNextPair,
-	topRunways
+	profile
 );
 
 publicRouter.use('/', (req, res) => {
