f744d1d0cf70f2f75eb732f1ff32260e84b00863
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-27 22:08:53 +0100

    Import routes without public/protectedRouters

--- server/src/app/app.ts
+++ server/src/app/app.ts
@@ -7,20 +7,27 @@ import express, {
 import helmet from 'helmet';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 import { connectToDatabase } from './database/database.js';
 import { corsOptions } from './middleware/cors.js';
-import publicRouter from './routes/publicRouter.js';
-import protectedRouter from './routes/protectedRouter.js';
-import expressWinston from 'express-winston';
 import { logger, expressLogger } from './middleware/logger.js';
 import { limiter } from './middleware/limiter.js';
 import { validateToken } from './middleware/jwtToken.js';
+
+import expressWinston from 'express-winston';
+import signOut from './router/routes/signOut.js';
+import deleteAccount from './router/routes/deleteAccount.js';
+import welcome from './router/routes/welcome.js';
+import createAccount from './router/routes/createAccount.js';
+import signIn from './router/routes/signIn.js';
+import getNextPair from './router/routes/getNextPair.js';
+import images from './router/routes/images.js';
+import profile from './router/routes/profile.js';
 
 const app = express();
 connectToDatabase();
@@ -29,14 +36,28 @@ app.use(express.static(path.join(__dirname, '../../public')));
 
 app.use(express.json(), cookieParser(), cors(corsOptions));
 
-app.use(validateToken);
-app.use('/', publicRouter, protectedRouter);
+app.use(images);
+
+app.use('/', validateToken, welcome);
+app.use('/', validateToken, createAccount);
+app.use('/', validateToken, signIn);
+app.use('/', validateToken, signOut);
+app.use('/', validateToken, getNextPair);
+app.use('/', validateToken, profile);
+
+app.use('/sign-out', validateToken, signOut);
+app.use('/delete-account', validateToken, deleteAccount);
 
 app.use(
 	expressWinston.errorLogger({
 		winstonInstance: logger,
 	})
 );
+
+app.use('/', (req, res) => {
+	res.status(404).json({ message: 'Route not found' });
+	logger.info(`Unmatched route: ${req.method} ${req.originalUrl}`);
+});
 
 const errorHandler: ErrorRequestHandler = (
 	err: Error,
