ce2a6ed7d6df43dfe5d3977771af86f9faa5c118
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-27 15:17:56 +0100

    Add logs, set cookie to secure

--- server/src/app/routes/protectedRoutes/deleteAccount.ts
+++ server/src/app/routes/protectedRoutes/deleteAccount.ts
@@ -1,28 +1,33 @@
 import express, { Router, Response } from 'express';
 
-import { environment } from '@/environment.js';
 import { UserModel } from '@/app/database/models/User.js';
-import { CustomRequest } from '@/types.js';
+import { CustomRequest } from '../../../../../types.js';
+import { logger } from '@/app/middleware/logger.js';
 
 export default express
 	.Router()
 	.delete('/delete-account', async (req: CustomRequest, res: Response) => {
+		logger.info('Delete account request received');
 		try {
 			const userId = req.user?.id;
 
 			if (!userId) {
+				logger.info('Unauthorized deletion attempt, missing user ID');
 				return res.status(401).json({ message: 'Unauthorized' });
 			}
 
+			logger.info(`Attempting to delete user with ID: ${userId}`);
 			const deletedUser = await UserModel.findByIdAndDelete(userId);
 
 			if (!deletedUser) {
+				logger.info(`User with ID: ${userId} not found`);
 				return res.status(404).json({ message: 'User not found' });
 			}
 
+			logger.info(`User with ID: ${userId} deleted successfully`);
 			res.clearCookie('jwt', {
 				httpOnly: true,
-				secure: environment.isProduction,
+				secure: true,
 				sameSite: 'none',
 			});
 
@@ -31,6 +36,7 @@ export default express
 			});
 		} catch (error) {
 			console.error('Error deleting account:', error);
+			logger.error('Error deleting account:', error);
 			res.status(500).json({
 				message: 'An error occurred while deleting the account',
 			});
