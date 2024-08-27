b1d44b705d2e8e04279d45daaf8e824fa3c0a888
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-27 15:11:31 +0100

    Update property names

--- server/src/app/database/models/Runway.ts
+++ server/src/app/database/models/Runway.ts
@@ -1,22 +1,22 @@
 import mongoose from 'mongoose';
 
-import { RunwayItem } from '@/types.js';
+import { RunwayItem } from '../../../../../types.js';
 
 const RunwaySchema = new mongoose.Schema<RunwayItem>({
 	name: { type: String, required: true },
-	queen_id: {
+	queenId: {
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'Queen',
 		required: true,
 	},
-	queen_name: { type: String, required: true },
+	queenName: { type: String, required: true },
 	franchise: String,
 	season: Number,
 	episode: Number,
-	episode_name: String,
+	episodeName: String,
 	score: { type: Number, default: 1600 },
-	ratings_count: { type: Number, default: 0 },
-	image_url: { type: String, required: true },
+	ratingsCount: { type: Number, default: 0 },
+	imageSlug: { type: String, required: true },
 });
 
 export const RunwayModel = mongoose.model<RunwayItem>('Runway', RunwaySchema);
