c94a799e74ca9107e7ccfe82a7713f9917cfec5c
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-27 15:17:36 +0100

    Update protectedRouter.ts

--- server/src/app/routes/protectedRouter.ts
+++ server/src/app/routes/protectedRouter.ts
@@ -2,10 +2,9 @@ import express, { Router } from 'express';
 
 import protectedRoute from './protectedRoutes/protectedRoute.js';
 import deleteAccount from './protectedRoutes/deleteAccount.js';
-import { validateToken } from '../middleware/jwtToken.js';
 
 const protectedRouter: Router = express.Router();
 
-protectedRouter.use('/', validateToken, protectedRoute, deleteAccount);
+protectedRouter.use('/accounts', deleteAccount);
 
 export default protectedRouter as Router;
