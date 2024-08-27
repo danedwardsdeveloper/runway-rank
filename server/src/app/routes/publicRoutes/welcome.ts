1132644b40e2cad4a7b7403ae0202e42bb53e72d
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-27 22:05:21 +0100

    Delete publicRouter

--- server/src/app/routes/publicRoutes/welcome.ts
+++
@@ -1,7 +0,0 @@
-import express, { Router, Request, Response } from 'express';
-
-export default express
-	.Router()
-	.get('/', (req: Request, res: Response): void => {
-		res.send('Welcome to the Runway Rank API');
-	}) as Router;
