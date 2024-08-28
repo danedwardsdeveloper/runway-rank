8e6bc7f3a2cc2d09e18183045863052d792e0780
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-28 17:47:04 +0100

    Get images from server

--- client/src/components/ImageContainer.tsx
+++ client/src/components/ImageContainer.tsx
@@ -28,8 +28,8 @@ export default function ImageContainer({
 						<div className="flex-grow flex flex-col justify-end mb-4 ">
 							<img
 								onClick={() => isAuthenticated && onImageClick(img)}
-								src={`/runways/${img.imageSlug}.webp`}
-								alt={img.name}
+								src={`http://localhost:3000/images/${img.imageSlug}.webp`}
+								alt={`${img.queenName} - ${img.name}`}
 								className={`w-full h-auto md:max-w-[400px] md:max-h-[400px] object-contain rounded-lg border-4 border-transparent transition-all duration-200 ${
 									isAuthenticated
 										? 'cursor-pointer hover:border-pink-500'
