66023dae43d4abcabd57f52ca815511a42f7e088
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-28 19:35:11 +0100

    Allow local server

--- server/src/app/middleware/cors.ts
+++ server/src/app/middleware/cors.ts
@@ -1,9 +1,10 @@
 import { CorsOptions } from 'cors';
 
 export const corsOptions: CorsOptions = {
-	origin: (origin, callback) => {
-		callback(null, true);
-	},
+	// origin: (origin, callback) => {
+	// 	callback(null, true);
+	// },
+	origin: 'http://localhost:5173',
 	methods: ['GET', 'POST', 'DELETE'],
 	allowedHeaders: ['Content-Type', 'Authorization'],
 	credentials: true,
