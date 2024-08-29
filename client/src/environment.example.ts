4acd355b2ef63514d99b4f0cc7c3001b928dfa42
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-29 11:46:19 +0100

    Create environment.example.ts

---
+++ client/src/environment.example.ts
@@ -0,0 +1,13 @@
+// const PRODUCTION_SERVER_URL = 'https://my-server.com';
+// const DEVELOPMENT_SERVER_URL = 'http://localhost:3000';
+
+// const isProduction: boolean = import.meta.env.MODE === 'production';
+
+// const apiBase: string = isProduction
+// 	? PRODUCTION_SERVER_URL
+// 	: DEVELOPMENT_SERVER_URL;
+
+// export const environment = {
+// 	apiBase: apiBase,
+// 	isProduction: isProduction,
+// };
